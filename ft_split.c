ff4867c039b286a9d2a1d51e37db67811270b86f
Radek Pelikan <rpelikan@c2r5s5.42prague.com>
2024-01-21 18:11:54 +0100

    Day five

--- ft_split.c
+++ ft_split.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft_split.c                                      :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpelikan <rpelikan@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 20:45:58 by rpelikan          #+#    #+#             */
-/*   Updated: 2024/01/15 21:00:59 by rpelikan         ###   ########.fr       */
+/*   Updated: 2024/01/21 18:10:51 by rpelikan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 char **ft_split(char const *s, char c) {
 	char **result;
 	char *find_ptr;
-	size_t len;
+	int len;
 	int i;
 
+	i = 0;
 	len = ft_strlen(s);
 	find_ptr = ft_strchr(s + i + 1,c);
 	i = find_ptr -  s;
-	while(s[i] && i < len){
+		while(s[i] && i < len){
+			printf("%d | %c\n", i, s[i]);
 		find_ptr = ft_strchr(s + i + 1,c);
 		i = find_ptr -  s;
 	}
